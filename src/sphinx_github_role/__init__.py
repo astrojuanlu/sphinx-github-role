2514b7dda587372abc611569cf00815f136e89b7
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-11-20 19:53:22 +0100

    Make prototype of role

--- src/sphinx_github_role/__init__.py
+++ src/sphinx_github_role/__init__.py
@@ -3,13 +3,13 @@ sphinx-github-role
 
 A github role for Sphinx
 """
-
 from __future__ import annotations
 
+import re
 from typing import TYPE_CHECKING, Any
 
 from docutils import nodes
-from sphinx.util.docutils import ReferenceRole
+from sphinx.util.docutils import SphinxRole
 
 if TYPE_CHECKING:
     from docutils.nodes import Node, system_message
@@ -18,10 +18,22 @@ if TYPE_CHECKING:
 __version__ = "0.1"
 
 
-class GitHub(ReferenceRole):
+class GitHub(SphinxRole):
+    # For example: org/proj#1
+    gh_re = re.compile(r"(?P<org>.+)/(?P<proj>.+)#(?P<num>\d+)")
+    # The /issues/{num} and /pull/{num} URLs automatically redirect
+    gh_tpl = "https://github.com/{org}/{proj}/issues/{num}"
+
     def run(self) -> tuple[list[Node], list[system_message]]:
-        paragraph_node = nodes.paragraph(text="GitHub!")
-        return [paragraph_node], []
+        # breakpoint()
+        match = self.gh_re.fullmatch(self.text)
+        node = nodes.reference(
+            self.rawtext,
+            self.text,
+            refuri=self.gh_tpl.format(**match.groupdict()),
+        )
+
+        return [node], []
 
 
 def setup(app: Sphinx) -> dict[str, Any]:
