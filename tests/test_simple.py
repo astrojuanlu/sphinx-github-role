fe9b8f2469a1f825f782de39670010a479d6a7e3
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-11-21 10:56:55 +0100

    Parametrize test to avoid duplication

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,42 +5,18 @@ import pytest
 from sphinx.testing.util import SphinxTestApp
 
 
-# TODO: Is there any way to parametrize a custom mark?
-# For example, repeat this test with
-# @pytest.mark.sphinx(testroot="root") and
-# @pytest.mark.sphinx(testroot="myst")
-def test(app: SphinxTestApp) -> None:
+# TODO: Awaiting for a nicer way to do this,
+# See https://stackoverflow.com/q/70053419/554319
+@pytest.mark.parametrize(
+    "_",
+    [
+        pytest.param(None, marks=pytest.mark.sphinx),
+        pytest.param(None, marks=pytest.mark.sphinx(testroot="myst")),
+    ],
+)
+def test_github_role_produces_html_hyperlink(app: SphinxTestApp, _: None) -> None:
     # app is a Sphinx application object for default sphinx project
     # (tests/cases/test-root)
-
-    # See https://github.com/sphinx-doc/sphinx/issues/7008#issuecomment-974680232
-    app.warningiserror = app.keep_going = True
-
-    app.build()
-
-    path = Path(app.outdir) / "index.html"
-    assert path.exists()
-
-    content = open(path).read()
-
-    chunks = [
-        (
-            '<a class="reference external" '
-            'href="https://github.com/readthedocs/readthedocs.org/issues/1">'
-            "readthedocs/readthedocs.org#1</a>"
-        ),
-    ]
-
-    for chunk in chunks:
-        assert chunk in content
-
-
-@pytest.mark.sphinx(testroot="myst")
-def test_myst(app: SphinxTestApp) -> None:
-    # app is a Sphinx application for myst sphinx project
-    # (tests/cases/test-myst)
-    app.warningiserror = app.keep_going = True
-
     app.build()
 
     path = Path(app.outdir) / "index.html"
