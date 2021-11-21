b82bccd2411449b38560533f5a569122013a9692
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-11-21 11:46:53 +0100

    Reuse test data in tests

--- tests/test_github_role.py
+++ tests/test_github_role.py
@@ -1,7 +1,20 @@
+from __future__ import annotations
+
 from pathlib import Path
 
 import pytest
 from sphinx.testing.util import SphinxTestApp
+
+
+@pytest.fixture
+def chunks():
+    return [
+        (
+            '<a class="reference external" '
+            'href="https://github.com/readthedocs/readthedocs.org/issues/1">'
+            "readthedocs/readthedocs.org#1</a>"
+        ),
+    ]
 
 
 # TODO: Awaiting for a nicer way to do this,
@@ -13,21 +26,15 @@ from sphinx.testing.util import SphinxTestApp
         pytest.param(None, marks=pytest.mark.sphinx(testroot="myst")),
     ],
 )
-def test_github_role_produces_html_hyperlink(app: SphinxTestApp, _: None) -> None:
+def test_github_role_produces_html_hyperlink(
+    app: SphinxTestApp, chunks: list[str], _: None
+) -> None:
     app.build()
 
     path = Path(app.outdir) / "index.html"
     assert path.exists()
 
     content = open(path).read()
-
-    chunks = [
-        (
-            '<a class="reference external" '
-            'href="https://github.com/readthedocs/readthedocs.org/issues/1">'
-            "readthedocs/readthedocs.org#1</a>"
-        ),
-    ]
 
     for chunk in chunks:
         assert chunk in content
